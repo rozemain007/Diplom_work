fab5a24cc7eca06c1a29e8f8fa7656d79f13f99b
rozemain007 <ivanovanastya1896@gmail.com>
2025-12-15 16:02:12 +0500

    Изменения методов

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,16 +1,22 @@
 import allure
 from selenium import webdriver
 from pages.method_ui import UImethods
+from config import MAIN_PAGE_URL
 
 
 @allure.title("Поиск товара - валидное значение")
 def test_search_val():
     driver = webdriver.Chrome()
     browser = UImethods(driver)
-    search_phrase = "Python"
-    browser.send_search_str(search_phrase)
-    text = browser.get_founded_book()
-    assert search_phrase in text
+    with allure.step("Перейти на страницу поиска"):
+        browser.go_to_main_page(MAIN_PAGE_URL)
+    with allure.step("Ввести поисковый запрос"):
+        search_phrase = "Python"
+        browser.send_search_str(search_phrase)
+    with allure.step("Получить название первой кнги"):
+        text = browser.get_founded_book()
+    with allure.step("Сравнить есть ли слово-запрос в названии"):
+        assert search_phrase in text
     driver.quit()
 
 
@@ -18,10 +24,15 @@ def test_search_val():
 def test_search_num():
     driver = webdriver.Chrome()
     browser = UImethods(driver)
-    search_phrase = "123456789"
-    browser.send_search_str(search_phrase)
-    text = browser.not_founded()
-    assert text == "Похоже, у нас такого нет"
+    with allure.step("Перейти на страницу поиска"):
+        browser.go_to_main_page(MAIN_PAGE_URL)
+    with allure.step("Ввести поисковый запрос"):
+        search_phrase = "123456789"
+        browser.send_search_str(search_phrase)
+    with allure.step("Получить ответ 'Похоже, у нас такого нет"):
+        text = browser.not_founded()
+    with allure.step("Сравнить, соответсвует ли результат ожидаемому"):
+        assert text == "Похоже, у нас такого нет"
     driver.quit()
 
 
@@ -29,10 +40,15 @@ def test_search_num():
 def test_search_simbols():
     driver = webdriver.Chrome()
     browser = UImethods(driver)
-    search_phrase = "저는 과제를 제출하고 싶습니다."
-    browser.send_search_str(search_phrase)
-    text = browser.not_founded()
-    assert text == "Похоже, у нас такого нет"
+    with allure.step("Перейти на страницу поиска"):
+        browser.go_to_main_page(MAIN_PAGE_URL)
+    with allure.step("Ввести поисковый запрос"):
+        search_phrase = "저는 과제를 제출하고 싶습니다."
+        browser.send_search_str(search_phrase)
+    with allure.step("Получить ответ 'Похоже, у нас такого нет"):
+        text = browser.not_founded()
+    with allure.step("Сравнить, соответствует ли результат ожидаемому"):
+        assert text == "Похоже, у нас такого нет"
     driver.quit()
 
 
@@ -40,11 +56,17 @@ def test_search_simbols():
 def test_add_to_basket():
     driver = webdriver.Chrome()
     browser = UImethods(driver)
-    search_phrase = "Python"
-    browser.send_search_str(search_phrase)
-    browser.add_book()
-    items = browser.busket()
-    assert items == "1 товар"
+    with allure.step("Перейти на страницу поиска"):
+        browser.go_to_main_page(MAIN_PAGE_URL)
+    with allure.step("Ввести поисковый запрос"):
+        search_phrase = "Python"
+        browser.send_search_str(search_phrase)
+    with allure.step("Добавить книгу в корзину"):
+        browser.add_book()
+    with allure.step("Получить кол-во товаров в корзине"):
+        items = browser.busket()
+    with allure.step("Сравнить полученное кол-во с ожидаемым"):
+        assert items == "1 товар"
     driver.quit()
 
 
@@ -52,9 +74,17 @@ def test_add_to_basket():
 def test_remove_book():
     driver = webdriver.Chrome()
     browser = UImethods(driver)
-    search_phrase = "Пайтон"
-    browser.send_search_str(search_phrase)
-    browser.add_book()
-    browser.remove_item()
-    no_found = browser.no_found()
-    assert no_found == "В корзине ничего нет"
+    with allure.step("Перейти на страницу поиска"):
+        browser.go_to_main_page(MAIN_PAGE_URL)
+    with allure.step("Ввести поисковый запрос"):
+        search_phrase = "Пайтон"
+        browser.send_search_str(search_phrase)
+    with allure.step('Добавить книгу в корзину'):
+        browser.add_book()
+    with allure.step("Удалить книгу из корзины"):
+        browser.remove_item()
+    with allure.step("Получить кол-во товаров в корзине"):
+        no_found = browser.no_found()
+    with allure.step("Убедиться что корзина пуста"):
+        assert no_found == "В корзине ничего нет"
+    driver.quit()
